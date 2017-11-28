38c17d129a4902ce0c55fb776ea3641e711c03d3
n.m.mobile <hihinuma@gmail.com>
2017-11-29 07:38:23 +0900

    add jsonp

---
+++ static/common/jsonp.js
@@ -0,0 +1,41 @@
+/* domUtil.js */
+/*eslint-env browser */
+/*eslint no-console: off */
+/*global define */
+
+
+define((require)=>{
+  const {
+    create,
+    head,
+  } = require('./domUtil');
+
+  function callJsonp(url,params) {
+    const callbackName = params.callback;
+    const paramStr = Object.entries(params).map((keyval)=>{
+      return keyval.join('=');
+    }).join('&');
+
+    const $script = create('script');
+    $script
+      .setAttr('type', 'text/javascript')
+      .setAttr('src', [url, paramStr].join('?'));
+    const prms = new Promise((resolve, reject)=>{
+      window[callbackName] = (data)=>{
+        resolve(data);
+        $script.remove();
+        // delete window[callbackName];
+      };
+      try {
+        head().append($script);
+      } catch (err) {
+        reject(err);
+      }
+    });
+    return prms;
+  }
+
+  return {
+    callJsonp
+  };
+});
